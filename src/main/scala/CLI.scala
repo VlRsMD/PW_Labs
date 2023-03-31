5efd63ac81d71dacc7ddd8c10b824b5f18f6a1d1
VlRsMD <90247966+VlRsMD@users.noreply.github.com>
2023-03-31 23:13:02 +0300

    Update

--- src/main/scala/CLI.scala
+++ src/main/scala/CLI.scala
@@ -2,10 +2,14 @@ import akka.actor.{Actor, ActorSystem, Props}
 import org.jsoup.Jsoup
 import org.jsoup.nodes.Document
 import org.jsoup.select.Elements
+import java.io.{BufferedInputStream, BufferedOutputStream, BufferedReader, IOException, InputStream, InputStreamReader, PrintStream}
 import scala.collection.mutable.ListBuffer
+import java.net.{ServerSocket, URL, URLConnection}
+import java.nio.charset.StandardCharsets
+import java.util.Scanner
 
 object search_engine {
-  def history_search = {
+  def history_search: List[String] = {
     val urlListB: ListBuffer[String] = new ListBuffer[String];
     urlListB += "https://www.history.com/"
     urlListB += "https://www.historytoday.com/archive/head-head/what-history"
@@ -18,11 +22,9 @@ object search_engine {
     urlListB += "https://www.goodreads.com/list/show/1362.Best_History_Books_"
     urlListB += "https://www.worldhistory.org/mapselect/"
     val urlList = urlListB.toList;
-    for (i: Int <-0 to urlList.length-1) {
-      println(urlList(i))
-    }
+    return urlList
   }
-  def science_search = {
+  def science_search: List[String] = {
     val urlListB: ListBuffer[String] = new ListBuffer[String];
     urlListB += "https://www.sciencenews.org/"
     urlListB += "https://www.nationalgeographic.com/science/"
@@ -35,11 +37,9 @@ object search_engine {
     urlListB += "https://www.sciencefocus.com/books/science-books-2/"
     urlListB += "https://www.sciencemadesimple.com/science-definition.html"
     val urlList = urlListB.toList;
-    for (i: Int <-0 to urlList.length-1) {
-      println(urlList(i))
-    }
+    return urlList
   }
-  def technology_search = {
+  def technology_search: List[String] = {
     val urlListB: ListBuffer[String] = new ListBuffer[String];
     urlListB += "https://www.britannica.com/technology/technology"
     urlListB += "https://www.bbc.com/news/technology"
@@ -52,113 +52,178 @@ object search_engine {
     urlListB += "https://www.indeed.com/career-advice/finding-a-job/types-of-technology"
     urlListB += "https://www.weforum.org/agenda/2020/11/heres-how-technology-has-changed-and-changed-us-over-the-past-20-years/"
     val urlList = urlListB.toList;
-    for (i: Int <-0 to urlList.length-1) {
-      println(urlList(i))
+    return urlList
+  }
+}
+
+object requestToUrl {
+  def get(urlString: String): Unit = {
+    val source = scala.io.Source.fromURL(urlString)
+    val content = source.mkString
+    var contentJsoupDoc: Document = Jsoup.parse(content)
+    val contentReadable: Elements = contentJsoupDoc.select("div")
+    val contentReadableArray =  contentReadable.toArray()
+    for (i<-0 until contentReadableArray.length) {
+      var contentDoc: Document = Jsoup.parse(contentReadableArray(i).toString)
+      var contentText: String = contentDoc.text()
+      println(contentText)
     }
   }
 }
 
 class CLI extends Actor {
   def receive = {
-    case List("go2web", "-u", url: String) => {
-      var doc: Document = Jsoup.connect(url).get();
-      val data :Elements = doc.select("div")
-      val dataListB: ListBuffer[String] = new ListBuffer[String]
-      val data_arr = data.toArray()
-      for (i<-0 to data_arr.length-1) {
-        var data_s: Document = Jsoup.parse(data_arr(i).toString)
-        var d: String = data_s.text()
-        dataListB += d
+    case List("go2web", "-u", urlString: String) => {
+      requestToUrl.get(urlString)
+      val cliActor = ActorSystem().actorOf(Props(new CLI))
+      var command: String = scala.io.StdIn.readLine()
+      var commandSplit = command.split(" ");
+      val commandSplitListBuffer: ListBuffer[String] = new ListBuffer[String]
+      for (i: Int <-0 to commandSplit.length-1) {
+        commandSplitListBuffer += commandSplit(i)
       }
-      val dataList = dataListB.toList
-      for (i<-0 to dataList.length-1) {
-        println(dataList(i))
-      }
-      val cli_actor = ActorSystem().actorOf(Props(new CLI))
-      var c: String = scala.io.StdIn.readLine()
-      var c_s = c.split(" ");
-      val c_s_LB: ListBuffer[String] = new ListBuffer[String]
-      for (i: Int <-0 to c_s.length-1) {
-        c_s_LB += c_s(i)
-      }
-      val c_s_l = c_s_LB.toList
-      cli_actor ! c_s_l
+      val commandSplitList = commandSplitListBuffer.toList
+      cliActor ! commandSplitList
     }
 
     case List("go2web", "-s", search_term: String) => {
       if (search_term.contains("history")) {
-        search_engine.history_search
-        val cli_actor = ActorSystem().actorOf(Props(new CLI))
-        var c: String = scala.io.StdIn.readLine()
-        var c_s = c.split(" ");
-        val c_s_LB: ListBuffer[String] = new ListBuffer[String]
-        for (i: Int <-0 to c_s.length-1) {
-          c_s_LB += c_s(i)
+        for(i<-search_engine.history_search.indices) {
+          println(search_engine.history_search(i))
         }
-        val c_s_l = c_s_LB.toList
-        cli_actor ! c_s_l
+        val cliActor = ActorSystem().actorOf(Props(new CLI))
+        var command: String = scala.io.StdIn.readLine()
+        var commandSplit = command.split(" ");
+        val commandSplitListBuffer: ListBuffer[String] = new ListBuffer[String]
+        for (i: Int <-0 to commandSplit.length-1) {
+          commandSplitListBuffer += commandSplit(i)
+        }
+        val commandSplitList = commandSplitListBuffer.toList
+        cliActor ! commandSplitList
       } else if (search_term.contains("science")) {
-        search_engine.science_search
-        val cli_actor = ActorSystem().actorOf(Props(new CLI))
-        var c: String = scala.io.StdIn.readLine()
-        var c_s = c.split(" ");
-        val c_s_LB: ListBuffer[String] = new ListBuffer[String]
-        for (i: Int <-0 to c_s.length-1) {
-          c_s_LB += c_s(i)
+        for(i<-search_engine.history_search.indices) {
+          println(search_engine.history_search(i))
         }
-        val c_s_l = c_s_LB.toList
-        cli_actor ! c_s_l
+        val cliActor = ActorSystem().actorOf(Props(new CLI))
+        var command: String = scala.io.StdIn.readLine()
+        var commandSplit = command.split(" ");
+        val commandSplitListBuffer: ListBuffer[String] = new ListBuffer[String]
+        for (i: Int <-0 to commandSplit.length-1) {
+          commandSplitListBuffer += commandSplit(i)
+        }
+        val commandSplitList = commandSplitListBuffer.toList
+        cliActor ! commandSplitList
       } else if (search_term.contains("technology")) {
-        search_engine.technology_search
-        val cli_actor = ActorSystem().actorOf(Props(new CLI))
-        var c: String = scala.io.StdIn.readLine()
-        var c_s = c.split(" ");
-        val c_s_LB: ListBuffer[String] = new ListBuffer[String]
-        for (i: Int <-0 to c_s.length-1) {
-          c_s_LB += c_s(i)
+        for(i<-search_engine.history_search.indices) {
+          println(search_engine.history_search(i))
         }
-        val c_s_l = c_s_LB.toList
-        cli_actor ! c_s_l
+        val cliActor = ActorSystem().actorOf(Props(new CLI))
+        var command: String = scala.io.StdIn.readLine()
+        var commandSplit = command.split(" ");
+        val commandSplitListBuffer: ListBuffer[String] = new ListBuffer[String]
+        for (i: Int <-0 to commandSplit.length-1) {
+          commandSplitListBuffer += commandSplit(i)
+        }
+        val commandSplitList = commandSplitListBuffer.toList
+        cliActor ! commandSplitList
       } else {
         println("Unable to find relevant resources")
-        val cli_actor = ActorSystem().actorOf(Props(new CLI))
-        var c: String = scala.io.StdIn.readLine()
-        var c_s = c.split(" ");
-        val c_s_LB: ListBuffer[String] = new ListBuffer[String]
-        for (i: Int <-0 to c_s.length-1) {
-          c_s_LB += c_s(i)
+        val cliActor = ActorSystem().actorOf(Props(new CLI))
+        var command: String = scala.io.StdIn.readLine()
+        var commandSplit = command.split(" ");
+        val commandSplitListBuffer: ListBuffer[String] = new ListBuffer[String]
+        for (i: Int <-0 to commandSplit.length-1) {
+          commandSplitListBuffer += commandSplit(i)
         }
-        val c_s_l = c_s_LB.toList
-        cli_actor ! c_s_l
+        val commandSplitList = commandSplitListBuffer.toList
+        cliActor ! commandSplitList
+      }
+    }
+
+    case List("go2web", "-s", "-u", search_term: String) => {
+      if (search_term.contains("history")) {
+        for(i<-search_engine.history_search.indices) {
+          println(search_engine.history_search(i))
+          requestToUrl.get(search_engine.history_search(i))
+        }
+        val cliActor = ActorSystem().actorOf(Props(new CLI))
+        var command: String = scala.io.StdIn.readLine()
+        var commandSplit = command.split(" ");
+        val commandSplitListBuffer: ListBuffer[String] = new ListBuffer[String]
+        for (i: Int <-0 to commandSplit.length-1) {
+          commandSplitListBuffer += commandSplit(i)
+        }
+        val commandSplitList = commandSplitListBuffer.toList
+        cliActor ! commandSplitList
+      } else if (search_term.contains("science")) {
+        for(i<-search_engine.history_search.indices) {
+          println(search_engine.history_search(i))
+          requestToUrl.get(search_engine.history_search(i))
+        }
+        val cliActor = ActorSystem().actorOf(Props(new CLI))
+        var command: String = scala.io.StdIn.readLine()
+        var commandSplit = command.split(" ");
+        val commandSplitListBuffer: ListBuffer[String] = new ListBuffer[String]
+        for (i: Int <-0 to commandSplit.length-1) {
+          commandSplitListBuffer += commandSplit(i)
+        }
+        val commandSplitList = commandSplitListBuffer.toList
+        cliActor ! commandSplitList
+      } else if (search_term.contains("technology")) {
+        for(i<-search_engine.history_search.indices) {
+          println(search_engine.history_search(i))
+          requestToUrl.get(search_engine.history_search(i))
+        }
+        val cliActor = ActorSystem().actorOf(Props(new CLI))
+        var command: String = scala.io.StdIn.readLine()
+        var commandSplit = command.split(" ");
+        val commandSplitListBuffer: ListBuffer[String] = new ListBuffer[String]
+        for (i: Int <-0 to commandSplit.length-1) {
+          commandSplitListBuffer += commandSplit(i)
+        }
+        val commandSplitList = commandSplitListBuffer.toList
+        cliActor ! commandSplitList
+      } else {
+        println("Unable to find relevant resources")
+        val cliActor = ActorSystem().actorOf(Props(new CLI))
+        var command: String = scala.io.StdIn.readLine()
+        var commandSplit = command.split(" ");
+        val commandSplitListBuffer: ListBuffer[String] = new ListBuffer[String]
+        for (i: Int <-0 to commandSplit.length-1) {
+          commandSplitListBuffer += commandSplit(i)
+        }
+        val commandSplitList = commandSplitListBuffer.toList
+        cliActor ! commandSplitList
       }
     }
 
     case List("go2web", "-h") => {
-      println("go2web -u <URL>                  print the response from the specified URL")
-      println("go2web -s <search-term>          search the specified term and print top 10 results")
-      println("go2web -h                        show  this help")
-      val cli_actor = ActorSystem().actorOf(Props(new CLI))
-      var c: String = scala.io.StdIn.readLine()
-      var c_s = c.split(" ");
-      val c_s_LB: ListBuffer[String] = new ListBuffer[String]
-      for (i: Int <-0 to c_s.length-1) {
-        c_s_LB += c_s(i)
+      println("go2web -u <URL>                      print the response from the specified URL")
+      println("go2web -s <search-term>              search the specified term and print top 10 results")
+      println("go2web -s -u <search-term>           search the specified term and print each of the top 10 results together with response from correspondent URL")
+      println("go2web -h                            show  this help")
+      val cliActor = ActorSystem().actorOf(Props(new CLI))
+      var command: String = scala.io.StdIn.readLine()
+      var commandSplit = command.split(" ");
+      val commandSplitListBuffer: ListBuffer[String] = new ListBuffer[String]
+      for (i: Int <-0 to commandSplit.length-1) {
+        commandSplitListBuffer += commandSplit(i)
       }
-      val c_s_l = c_s_LB.toList
-      cli_actor ! c_s_l
+      val commandSplitList = commandSplitListBuffer.toList
+      cliActor ! commandSplitList
     }
   }
 }
 
 object cli_app extends App {
   val system = ActorSystem()
-  val cli_actor = system.actorOf(Props(new CLI))
-  var c: String = scala.io.StdIn.readLine()
-  var c_s = c.split(" ");
-  val c_s_LB: ListBuffer[String] = new ListBuffer[String]
-  for (i: Int <-0 to c_s.length-1) {
-    c_s_LB += c_s(i)
+  val cliActor = ActorSystem().actorOf(Props(new CLI))
+  var command: String = scala.io.StdIn.readLine()
+  var commandSplit = command.split(" ");
+  val commandSplitListBuffer: ListBuffer[String] = new ListBuffer[String]
+  for (i: Int <-0 to commandSplit.length-1) {
+    commandSplitListBuffer += commandSplit(i)
   }
-  val c_s_l = c_s_LB.toList
-  cli_actor ! c_s_l
+  val commandSplitList = commandSplitListBuffer.toList
+  cliActor ! commandSplitList
 }
