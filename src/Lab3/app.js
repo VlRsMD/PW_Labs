faf5ed26e5f7ca2aab18a440b71f0c585a9a05f9
VlRsMD <90247966+VlRsMD@users.noreply.github.com>
2023-04-07 17:58:56 +0300

    Update

--- src/Lab3/app.js
+++ src/Lab3/app.js
@@ -1,59 +1,58 @@
-var taskList = document.getElementsByTagName("LI");
+const taskList = document.getElementsByTagName("LI");
 var i;
 for (i = 0; i < taskList.length; i++) {
-    var span = document.createElement("SPAN");
-    var txt = document.createTextNode("\u2716");
+    const span = document.createElement("SPAN");
+    const txt = document.createTextNode("\u2716");
     span.className = "close";
     span.appendChild(txt);
     taskList[i].appendChild(span);
 }
-
-var close = document.getElementsByClassName("close");
+const close = document.getElementsByClassName("close");
 var i;
 for (i = 0; i < close.length; i++) {
     close[i].onclick = function() {
-        var div = this.parentElement;
+        const div = this.parentElement;
         div.style.display = "none";
     }
 }
 
 var i;
 for (i = 0; i < taskList.length; i++) {
-    var span = document.createElement("SPAN");
-    var txt = document.createTextNode("\u2714");
+    const span = document.createElement("SPAN");
+    const txt = document.createTextNode("\u2714");
     span.className = "done";
     span.appendChild(txt);
     taskList[i].appendChild(span);
 }
 
-var done = document.getElementsByClassName("done");
+const done = document.getElementsByClassName("done");
 var i;
 for (i = 0; i < done.length; i++) {
     done[i].onclick = function() {
-        var div = this.parentElement;
+        const div = this.parentElement;
         div.style.display = "none";
     }
 }
 
-var list = document.querySelector('ul');
+const list = document.querySelector('ul');
 list.addEventListener('click', function(ev) {
     if (ev.target.className === 'done') {
         ev.target.style.display = "none";
-        var newLi = document.createElement("li");
-        var task = event.target.parentElement.getElementsByClassName("taskTitle");
-        var taskTitle = task.item(0).firstChild.nodeValue
-        var taskText = document.createTextNode(taskTitle);
+        const newLi = document.createElement("li");
+        const task = event.target.parentElement.getElementsByClassName("taskTitle");
+        const taskTitle = task.item(0).firstChild.nodeValue
+        const taskText = document.createTextNode(taskTitle);
         newLi.appendChild(taskText);
         document.getElementById("list2").appendChild(newLi);
-        var span = document.createElement("SPAN");
-        var txt = document.createTextNode("\u2716");
+        const span = document.createElement("SPAN");
+        const txt = document.createTextNode("\u2716");
         span.className = "close";
         span.appendChild(txt);
         newLi.appendChild(span);
 
         for (i = 0; i < close.length; i++) {
             close[i].onclick = function() {
-                var div = this.parentElement;
+                const div = this.parentElement;
                 div.style.display = "none";
             }
         }
@@ -61,39 +60,37 @@ list.addEventListener('click', function(ev) {
 }, false);
 
 function newElement() {
-    var li = document.createElement("li");
-
-    var task = document.createElement("h3");
-    var taskInput = document.getElementById("taskInput").value;
-    var tI = document.createTextNode(taskInput);
+    const li = document.createElement("li");
+    const task = document.createElement("h3");
+    const taskInput = document.getElementById("taskInput").value;
+    const tI = document.createTextNode(taskInput);
     task.appendChild(tI);
     li.appendChild(task)
 
-    var description = document.createElement("h4");
-    var descriptionInput = document.getElementById("descriptionInput").value;
-    var dI = document.createTextNode(descriptionInput);
+    const description = document.createElement("h4");
+    const descriptionInput = document.getElementById("descriptionInput").value;
+    const dI = document.createTextNode(descriptionInput);
     description.appendChild(dI);
     li.appendChild(description)
 
-    var time = document.createElement("div");
-    var timeInput = document.getElementById("timeInput").value;
-    var timeI = document.createTextNode(timeInput);
+    const time = document.createElement("div");
+    const timeInput = document.getElementById("timeInput").value;
+    const timeI = document.createTextNode(timeInput);
     time.appendChild(timeI);
     li.appendChild(time)
 
     document.getElementById("list1").appendChild(li);
-
     document.getElementById("taskInput").value = "";
     document.getElementById("descriptionInput").value = "";
     document.getElementById("timeInput").value = "";
 
-    var span = document.createElement("SPAN");
-    var txt = document.createTextNode("\u2716");
-    span.className = "close";
-    span.appendChild(txt);
-    li.appendChild(span);
+    const spanClose = document.createElement("SPAN");
+    const txtClose = document.createTextNode("\u2716");
+    spanClose.className = "close";
+    spanClose.appendChild(txtClose);
+    li.appendChild(spanClose);
     for (i = 0; i < close.length; i++) {
-        close[i].onclick = function() {
+        close[i].onclick = function () {
             var div = this.parentElement;
             div.style.display = "none";
         }
@@ -101,16 +98,16 @@ function newElement() {
     var i;
     for (i = 0; i < taskList.length; i++) {
         if (taskList[i].parentElement.className === 'unorderedListToDo') {
-            var span = document.createElement("SPAN");
-            var txt = document.createTextNode("\u2714");
-            span.className = "done";
-            span.appendChild(txt);
-            taskList[i].appendChild(span);
+            const spanDone = document.createElement("SPAN");
+            const txtDone = document.createTextNode("\u2714");
+            spanDone.className = "done";
+            spanDone.appendChild(txtDone);
+            taskList[i].appendChild(spanDone);
         }
     }
     var i;
     for (i = 0; i < done.length; i++) {
-        done[i].onclick = function() {
+        done[i].onclick = function () {
             var div = this.parentElement;
             div.style.display = "none";
         }
